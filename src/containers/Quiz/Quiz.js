f6ae8744528e0df7d5195b02dc5ee7ee665c0802
Arthur Harmash <usd@arturs-mbp.home>
2022-05-15 12:51:41 +0200

    first commit

---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,33 @@
+import React, {Component} from "react";
+import classes from "./Quiz.module.css";
+import ActiveQuiz from "../../ActiveQuiz/ActiveQuiz";
+
+
+export default class Quiz extends Component{
+    state = {
+        quiz: [
+            {
+                answers: [
+                    {text: 'question 1'},
+                    {text: 'question 2'},
+                    {text: 'question 3'},
+                    {text: 'question 4'},
+                ]
+            }
+        ]
+    }
+    render(){
+        return(
+            <div className={classes.Quiz}>
+                <div className={classes.QuizWrapper}>
+                    <h1>
+                        Answer all the questions
+                    </h1>
+                    <ActiveQuiz
+                    answers = {this.state.quiz[0].answers}
+                    />
+                </div>
+            </div>
+        )
+    }
+}
