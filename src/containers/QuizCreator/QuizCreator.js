bff1d5b5c8be986325f72987c2c4b7e1d4ffc8d2
usd <arturharmash@gmail.com>
2022-05-30 18:21:32 +0200

    Lesson 83

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -43,10 +43,37 @@ export default class QuizCreator extends Component {
     addQuestionHandler = (event) => {
         event.preventDefault()
 
+        const quiz = this.state.quiz.concat()
+        const index = quiz.length + 1
+
+        const {question, option3, option4, option2, option1} = this.state.formControls
+
+        const questionItem = {
+            question: question.value,
+            id: index,
+            rightAnswerId: this.state.rightAnswerId,
+            answers: [
+                {text: option1.value, id: option1.id},
+                {text: option2.value, id: option2.id},
+                {text: option3.value, id: option3.id},
+                {text: option4.value, id: option4.id}
+            ]
+        }
+
+        quiz.push(questionItem)
+
+        this.setState({
+            quiz,
+            isFormValid: false,
+            rightAnswerId: 1,
+            formControls: createFormControls()
+        })
     }
 
-    createQuizHandler = () => {
+    createQuizHandler = event => {
+        event.preventDefault()
 
+        console.log(this.state.quiz)
     }
 
     changeHandler = (value, controlName) => {
@@ -97,10 +124,10 @@ export default class QuizCreator extends Component {
             value={this.state.rightAnswerId}
             onChange={this.selectChangeHandler}
             options={[
-                {text:1, value: 1},
-                {text:2, value: 2},
-                {text:3, value: 3},
-                {text:4, value: 4},
+                {text: 1, value: 1},
+                {text: 2, value: 2},
+                {text: 3, value: 3},
+                {text: 4, value: 4},
 
             ]}
         />
